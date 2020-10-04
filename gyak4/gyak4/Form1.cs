4daee7a7bdc11feb41e83c15b23c6c549c80a1b1
Bálint Bak <balint.bak@stud.uni-corvinus.hu>
2020-10-04 21:57:26 +0200

    Excel library + CreateExcel Method

--- gyak4/gyak4/Form1.cs
+++ gyak4/gyak4/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using Microsoft.Office.Interop.Excel;
 
 namespace gyak4
 {
@@ -14,10 +17,42 @@ namespace gyak4
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            
+            CreateExcel(ref xlApp, ref xlWB, ref xlSheet);
+        }
+
+        private static void CreateExcel(ref Excel.Application xlApp, ref Workbook xlWB, ref Worksheet xlSheet)
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+
+                // CreateTable(); 
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         private void LoadData()
