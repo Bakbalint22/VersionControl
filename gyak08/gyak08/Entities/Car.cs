7751ac89a89de5326f4a3a1ece3e3274002f6c89
Bálint Bak <balint.bak@stud.uni-corvinus.hu>
2020-11-15 22:10:22 +0100

    Task7

---
+++ gyak08/gyak08/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using gyak08.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak08.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
