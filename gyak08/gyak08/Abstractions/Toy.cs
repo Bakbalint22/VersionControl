56f6c394d044dc90b2d8c9f89f0b145718a42207
Bálint Bak <balint.bak@stud.uni-corvinus.hu>
2020-11-15 22:04:19 +0100

    Task5

---
+++ gyak08/gyak08/Abstractions/Toy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak08.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+
+
+        protected abstract void DrawImage(Graphics g);
+        
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
